68444b6686be8edd7b60eacb58613d22c1f9f3ad
小海 <xiaohai@xiaohaideMacBook-Pro.local>
2017-12-29 09:53:03 +0800

    加入图片

--- data/data.js
+++ data/data.js
@@ -3,7 +3,7 @@ var local_database = [
     name: "design",
     title: "您有个人的百度百科介绍吗？",
     imgSrc: "/images/avatar.jpg",
-    bgImg: "/images/avatar/1.png",
+    bgImg: "/images/bg.jpg",
     answer: [
       {
         title: '有',
@@ -19,7 +19,7 @@ var local_database = [
     name: "design",
     title: "您是否开通个人新浪微博账号，并通过认证（即加黄V认证）？",
     imgSrc: "/images/avatar.jpg",
-    bgImg: "/images/avatar/1.png",
+    bgImg: "/images/bg.jpg",
     answer: [
       {
         title: '是',
@@ -35,7 +35,7 @@ var local_database = [
     name: "design",
     title: "近30天，您更新个人微博的情况是？",
     imgSrc: "/images/avatar.jpg",
-    bgImg: "/images/avatar/1.png",
+    bgImg: "/images/bg2.jpg",
     answer: [
       {
         title: '0-5次',
@@ -59,7 +59,7 @@ var local_database = [
     name: "design",
     title: "您是否开通个人微信账号，并保持日常更新朋友圈？",
     imgSrc: "/images/avatar.jpg",
-    bgImg: "/images/avatar/1.png",
+    bgImg: "/images/bg.jpg",
     answer: [
       {
         title: '是',
@@ -75,7 +75,7 @@ var local_database = [
     name: "design",
     title: "近30天，您发布朋友圈的情况是？",
     imgSrc: "/images/avatar.jpg",
-    bgImg: "/images/avatar/1.png",
+    bgImg: "/images/bg2.jpg",
     answer: [
       {
         title: '0-5次',
@@ -99,7 +99,7 @@ var local_database = [
     name: "design",
     title: "您在朋友圈及微博上发布的内容通常以什么为主？",
     imgSrc: "/images/avatar.jpg",
-    bgImg: "/images/avatar/1.png",
+    bgImg: "/images/bg2.jpg",
     answer: [
       {
         title: '个人生活记录',
@@ -123,7 +123,7 @@ var local_database = [
     name: "design",
     title: "您是否在知乎、论坛、贴吧等平台拥有资深账号？",
     imgSrc: "/images/avatar.jpg",
-    bgImg: "/images/avatar/1.png",
+    bgImg: "/images/bg.jpg",
     answer: [
       {
         title: '是',
@@ -139,7 +139,7 @@ var local_database = [
     name: "design",
     title: "您是否在今日头条、一点资讯等平台开通个人自媒体账号？",
     imgSrc: "/images/avatar.jpg",
-    bgImg: "/images/avatar/1.png",
+    bgImg: "/images/bg.jpg",
     answer: [
       {
         title: '是',
@@ -155,7 +155,7 @@ var local_database = [
     name: "self",
     title: "您是否拍摄过用于工作展示的个人形象风采照？",
     imgSrc: "/images/avatar.jpg",
-    bgImg: "/images/avatar/1.png",
+    bgImg: "/images/bg.jpg",
     answer: [
       {
         title: '是',
@@ -171,7 +171,7 @@ var local_database = [
     name: "trench",
     title: "近半年，您接受过多少次电视台、广播电台或网络视频媒体（如爱奇艺、优酷等）的采访？",
     imgSrc: "/images/avatar.jpg",
-    bgImg: "/images/avatar/1.png",
+    bgImg: "/images/bg2.jpg",
     answer: [
       {
         title: '0次',
@@ -195,7 +195,7 @@ var local_database = [
     name: "trench",
     title: "您是否被秒拍、优酷、爱奇艺等视频平台报道过个人新闻动态？",
     imgSrc: "/images/avatar.jpg",
-    bgImg: "/images/avatar/1.png",
+    bgImg: "/images/bg.jpg",
     answer: [
       {
         title: '是',
@@ -211,7 +211,7 @@ var local_database = [
     name: "trench",
     title: "近一年，您获得网络媒体报道多少篇个人相关新闻？",
     imgSrc: "/images/avatar.jpg",
-    bgImg: "/images/avatar/1.png",
+    bgImg: "/images/bg2.jpg",
     answer: [
       {
         title: '0篇',
@@ -235,7 +235,7 @@ var local_database = [
     name: "trench",
     title: "您是否接受过报纸、杂志的专访？",
     imgSrc: "/images/avatar.jpg",
-    bgImg: "/images/avatar/1.png",
+    bgImg: "/images/bg.jpg",
     answer: [
       {
         title: '是',
@@ -251,7 +251,7 @@ var local_database = [
     name: "trench",
     title: "据您了解，在搜索引擎前5页，是否有个人负面新闻？",
     imgSrc: "/images/avatar.jpg",
-    bgImg: "/images/avatar/1.png",
+    bgImg: "/images/bg.jpg",
     answer: [
       {
         title: '是',
@@ -267,7 +267,7 @@ var local_database = [
     name: "trench",
     title: "您是否以其他形式发表过个人对行业的观点，如微信专栏、出版图书等？",
     imgSrc: "/images/avatar.jpg",
-    bgImg: "/images/avatar/1.png",
+    bgImg: "/images/bg.jpg",
     answer: [
       {
         title: '是',
@@ -283,7 +283,7 @@ var local_database = [
     name: "diffuse",
     title: "目前，您是否在行业协会担任职务？",
     imgSrc: "/images/avatar.jpg",
-    bgImg: "/images/avatar/1.png",
+    bgImg: "/images/bg.jpg",
     answer: [
       {
         title: '是',
@@ -299,7 +299,7 @@ var local_database = [
     name: "diffuse",
     title: "近半年，您是否在行业大型会议上发表过讲话？",
     imgSrc: "/images/avatar.jpg",
-    bgImg: "/images/avatar/1.png",
+    bgImg: "/images/bg.jpg",
     answer: [
       {
         title: '是',
@@ -315,7 +315,7 @@ var local_database = [
     name: "diffuse",
     title: "近半年，您是否经常参加行业有影响力的活动？",
     imgSrc: "/images/avatar.jpg",
-    bgImg: "/images/avatar/1.png",
+    bgImg: "/images/bg.jpg",
     answer: [
       {
         title: '是',
@@ -331,7 +331,7 @@ var local_database = [
     name: "diffuse",
     title: "您参加的活动大多是多少人规模的？",
     imgSrc: "/images/avatar.jpg",
-    bgImg: "/images/avatar/1.png",
+    bgImg: "/images/bg2.jpg",
     answer: [
       {
         title: '20-50人',
@@ -355,7 +355,7 @@ var local_database = [
     name: "diffuse",
     title: "您个人或参与的项目、团队是否在行业内获得过奖项？",
     imgSrc: "/images/avatar.jpg",
-    bgImg: "/images/avatar/1.png",
+    bgImg: "/images/bg.jpg",
     answer: [
       {
         title: '是',
--- pages/end/end.js
+++ pages/end/end.js
@@ -1,3 +1,6 @@
+var wxCharts = require('../../utils/wxcharts.js');
+var app = getApp();
+var pieChart = null;
 Page({
   onShareAppMessage: function () {
     return {
@@ -46,12 +49,12 @@ Page({
     }
 
     
-    console.log(index)
-    console.log('design' + design)
-    console.log('self' + self)
-    console.log('trench' + trench)
-    console.log('diffuse' + diffuse)
-    console.log('total' +total)
+    // console.log(index)
+    // console.log('design' + design)
+    // console.log('self' + self)
+    // console.log('trench' + trench)
+    // console.log('diffuse' + diffuse)
+    // console.log('total' +total)
   
     this.setData({
       design: design,
@@ -60,6 +63,82 @@ Page({
       diffuse: diffuse,
       total: total
     })
+    this.totalText(total)
+    this.pie(design, self, trench, diffuse, total)
+  },
+  totalText: function (total) {
+    console.log(total)
+    var that = this
+    if (Math.abs(total) == 0) {
+      that.setData({
+        totalText: '老铁，啥都不说了，快来找魔豆的品牌魔法师吧！'
+      })
+    } 
+    if (parseInt(total) >= parseInt(1) && parseInt(total)  <= parseInt(30))   {
+      that.setData({
+        totalText: '您的个人品牌传播力还不够哦，或许您需要品牌魔法师为您制定专业提升秘术。'
+      })
+    } 
+    
+    if (parseInt(total) >= parseInt(31) && parseInt(total) <= parseInt(59)) {
+      that.setData({
+        totalText: '看您骨骼清奇，已初步具备个人品牌传播意识，稍加修炼，必然能全面扩大您的个人品牌影响力!'
+      })
+    } 
+    if (parseInt(total) >= parseInt(60) && parseInt(total) <= parseInt(75)) {
+      that.setData({
+        totalText: '您的个人品牌传播力不错哦，只是传播力度不够均衡，稍加调整必然能有更大的传播效果!'
+      })
+    } 
+    if (parseInt(total) >= parseInt(76) && parseInt(total) <= parseInt(89)) {
+      that.setData({
+        totalText: '哇哦，您的个人品牌传播力已经很强了！若能针对领域进一步强化就更好了，看好你！'
+      })
+    } 
+    if (parseInt(total) >= parseInt(90) && parseInt(total) <= parseInt(100)) {
+      that.setData({
+        totalText: '您的个人品牌传播力简直战斗值爆表！有没有兴趣与品牌魔法师一同进攻下一阶段？'
+      })
+    } 
+  },
+  /**
+   * 画饼
+   */
+  pie: function (design, self, trench, diffuse, total) {
+    // var design = 50
+    // var self = 50
+    // var trench = 50
+    // var diffuse = 50
+
+    var windowWidth = 300;
+    try {
+      var res = wx.getSystemInfoSync();
+      windowWidth = res.windowWidth;
+    } catch (e) {
+      console.error('getSystemInfoSync failed!');
+    }
+
+    pieChart = new wxCharts({
+      animation: true,
+      canvasId: 'pieCanvas',
+      type: 'pie',
+      series: [{
+        name: '平台建设',
+        data: design,
+      }, {
+        name: '个人形象传播',
+        data: self,
+        }, {
+          name: '渠道传播',
+          data: trench,
+        },{
+        name: '行业传播',
+        data: diffuse,
+      }],
+      width: 250,
+      height: 250,
+      dataLabel: true,
+    });
   },
   startTap: function () {
     wx.setStorageSync('choice_index', 0);
